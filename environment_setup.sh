0ec29f8300f4bf7029c4363129517ea2b6c74606
lordjea <lordjea@yahoo.com.mx>
2021-08-11 15:35:55 +0200

    Create environment_setup.sh

---
+++ environment_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Script to setup the environment to test directly in the container
+
+echo "Update local package repository"
+apk update 
+echo "Install required apps"
+apk add git bash curl
